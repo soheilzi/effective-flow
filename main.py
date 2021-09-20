c33c325647394fee940125ceaa12682a70fc7569
soheilzi <soheil.zibakhsh@gmail.com>
2021-09-20 15:10:15 +0430

    multithreading(multiprocessing) completed!

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import networkx as nx
+import multiprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
 import pylab
 import random
 import numpy as np
-import threading
 import time
 
-sampleSize = 64
-threadCount = 4
-X = [[0 for j in range(sampleSize // threadCount)] for i in range(threadCount)]
+sampleSize = 256
+processCount = 4
+X = [[multiprocessing.Value("f", 0.0, lock=False) for j in range(sampleSize // processCount)] for i in range(processCount)]
 
-def efSampler(G, randS, randT, threadId, costCoef=1):
-    for i in range(sampleSize // threadCount):
+def efSampler(G, randS, randT, processId, costCoef=1):
+    # print("%d, size = %d"% (processId, len(randS)))
+    for i in range(sampleSize // processCount):
+        # print("%d, i = %d"% (processId, i))
         s, t = randS[i], randT[i]
         mincostFlow = nx.max_flow_min_cost(G, s, t)
         mincost = nx.cost_of_flow(G, mincostFlow)
@@ -24,7 +26,8 @@ def efSampler(G, randS, randT, threadId, costCoef=1):
         fund = sum((G[s][u]['capacity'] for u in G.successors(s)))
         if fund == 0:
             continue
-        X[threadId][i] = (mincostFlowValue - costCoef * mincost) / fund
+        X[processId][i].value = (mincostFlowValue - costCoef * mincost) / fund
+        # print(X[processId][i])
     
 
 data = pd.read_csv("cloth/edges_ln.csv")
@@ -41,25 +44,34 @@ data = data.astype({'weight' : int})
 ### graph constructoin
 G = nx.from_pandas_edgelist(data, 'from_node_id', 'to_node_id', edge_attr=['capacity', 'weight'], create_using=nx.DiGraph())
 
-# multithreading
+# multiprocessing
 randS = random.sample(nodeList, sampleSize)
 randT = random.sample(nodeList, sampleSize)
 
-t0 = threading.Thread(target=efSampler, args=(G, randS[0:sampleSize // 4], randT[0:sampleSize // 4], 0,))
-t1 = threading.Thread(target=efSampler, args=(G, randS[sampleSize // 4: sampleSize // 2], randT[sampleSize // 4: sampleSize // 2], 1,))
-t2 = threading.Thread(target=efSampler, args=(G, randS[sampleSize // 2: 3 * sampleSize // 4], randT[sampleSize // 2: 3 * sampleSize // 4], 2,))
-t3 = threading.Thread(target=efSampler, args=(G, randS[3 * sampleSize // 4: sampleSize], randT[3 * sampleSize // 4: sampleSize], 3,))
+# pool = processPool(processCount)
+# r = pool.map(efSampler, (G, randS[0:sampleSize // processCount], randT[0:sampleSize // processCount], 0,))
+# pool.close()
+# pool.join()
 
-t0.start()
-t1.start()
-t2.start()
-t3.start()
+p0 = multiprocessing.Process(target=efSampler, args=(G, randS[0:sampleSize // processCount], randT[0:sampleSize // processCount], 0,))
+p1 = multiprocessing.Process(target=efSampler, args=(G, randS[sampleSize // processCount: 2 * sampleSize // processCount], randT[sampleSize // processCount: 2 * sampleSize // processCount], 1,))
+p2 = multiprocessing.Process(target=efSampler, args=(G, randS[sampleSize // 2: 3 * sampleSize // processCount], randT[sampleSize // 2: 3 * sampleSize // processCount], 2,))
+p3 = multiprocessing.Process(target=efSampler, args=(G, randS[3 * sampleSize // processCount: sampleSize], randT[3 * sampleSize // processCount: sampleSize], 3,))
 
-t0.join()
-t1.join()
-t2.join()
-t3.join()
+p0.start()
+p1.start()
+p2.start()
+p3.start()
 
-Xjoined = sum(X, [])
-print("overall std :", np.std(Xjoined) / np.sqrt(sampleSize))
+p0.join()
+p1.join()
+p2.join()
+p3.join()
+
+Xjoined = []
+for i in range(processCount):
+    for j in range(sampleSize // processCount):
+        Xjoined.append(X[i][j].value)
+
+print("overall std :", (np.std(Xjoined) / np.sqrt(sampleSize)))
 print("Lightning network EF :", np.mean(Xjoined))
