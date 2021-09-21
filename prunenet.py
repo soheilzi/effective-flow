b5df622521ab3a1413240b159ce7b1fb66aae5a5
soheilzi <soheil.zibakhsh@gmail.com>
2021-09-21 09:49:35 +0430

    prunenet completed and tested

--- prunenet.py
+++ prunenet.py
@@ -4,16 +4,12 @@ import random
 
 def remove_row_channels(file, channelsDelNum):
     df = pd.read_csv(file)
-    # print(df.size)
-    # args = random.sample([i for i in range(df.size)], channelsDelNum)
-    args = [266189, 81573, 137605]
-    print(args)
-    print(df.size)
+    args = random.sample(df.index.values.tolist(), channelsDelNum)
     deletedRows = df.iloc[args]
     print(deletedRows)
-    # df = df.drop(index=args)
-    # df.to_csv(file, index=False)
-    # return deletedRows['id']
+    df = df.drop(index=args)
+    df.to_csv(file, index=False)
+    return deletedRows['id']
 
 def remove_specific_row_from_csv(file, column_name, args):
     df = pd.read_csv(file)
@@ -26,8 +22,4 @@ channelsDelNum = int(sys.argv[1])
 channelsFile = sys.argv[2]
 edgesFile = sys.argv[3]
 
-
-# randIndex = random.sample(range(0, len()), channelsDelNum)
-
-# remove_specific_row_from_csv(edgesFile, "channel_id", remove_row_channels(channelsFile, channelsDelNum))
-remove_row_channels(channelsFile, channelsDelNum)
+remove_specific_row_from_csv(edgesFile, "channel_id", remove_row_channels(channelsFile, channelsDelNum))
